9364f0e8e684de81b3f00c70480f97f14230ecf0
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-09-02 15:16:16 +0300

    Pract task 01 done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,44 @@
 ﻿using System.Text;
+using System.Collections;
 namespace ConsoleApp020923
 {
+//Завдання 1
+//Створіть клас «Алфавіт», в якому мають міститися
+//літери англійського алфавіту.Реалізуйте підтримку іте -
+//ратора.Протестуйте можливість використання foreach
+//для класу «Алфавіт».
+
+    public class Alphabet
+    {
+        List<char> alphabet = new List<char>();
+        public Alphabet()
+        {
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                alphabet.Add(i);
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return alphabet.GetEnumerator();
+        }   
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HW/Mod__/ex01\n");
+            Console.WriteLine("HW/Mod10/Pract/ex01\n");
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+
+            Alphabet alphabet = new Alphabet();
+            foreach (char letter in alphabet)
+            {
+                Console.Write(letter + " ");
+            }
+            
+            Console.WriteLine("\n\nPress any key to exit...");
+            Console.ReadKey();
 
         }
     }
